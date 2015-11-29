62dfe7797edcf9b12368d3199b071f972ceb7e38
Robert Schmidt <rschmidt@smu.edu>
2015-11-28 19:07:51 -0600

    Finished if statement for solving command line problem

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -3,3 +3,10 @@
 asm_filename = ARGV[0]
 
 puts asm_filename
+
+if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
+  puts ARGV[0]
+end
+
+#and the argument ends in .asm
+#then we're good.
