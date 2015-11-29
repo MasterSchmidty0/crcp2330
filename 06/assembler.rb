be440092b9d981029922e9093f57b44fc804957d
Robert Schmidt <rschmidt@smu.edu>
2015-11-28 20:43:36 -0600

    Up to Project 6.6.1

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,5 +1,18 @@
 #! /usr/bin/env ruby
 
+class Assembler
+
+	def initialize(asm_file, hack_file)
+		@asm_file = asm_file
+		@hack_file = hack_file
+	end
+
+	def assemble!
+		puts @asm_file.read
+	end
+
+end
+
 def args_valid?
 	ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
 end
@@ -24,7 +37,7 @@ unless is_readable?(asm_filename)
 	abort("#{asm_filename} not found or is unreadable")
 end
 
-File.open(asm_filename, 'r') do |asm_file|
+File.open(asm_filename) do |asm_file|
 	File.open(hack_filename(asm_filename), 'w') do |hack_file|
 		assembler = Assembler.new(asm_file, hack_file)
 		assembler.assemble!
